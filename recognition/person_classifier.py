e6f53b2b7a80ac4ecb85e470363cb38089899f68
TakakiNishio <p104314t@mail.kyutech.jp>
2017-09-12 07:03:41 +0900

    [add] YOLOv2 + PersonClassifier

---
+++ recognition/person_classifier.py
@@ -0,0 +1,47 @@
+#python library
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy.random import *
+import time
+
+#openCV
+import cv2
+
+#chainer library
+import chainer
+import chainer.functions as F
+import chainer.links as L
+from chainer import training
+from chainer import serializers
+
+#python script
+import network_structure as nn
+import load_dataset as ld
+import visualizer as v
+
+
+# load_person classifier
+class PersonClassifier:
+    def __init__(self):
+
+        model_path = 'thibault_model2/'
+        model_name = 'cnn_gpu.model'
+        model = nn.CNN_thibault()
+        self.size = 50
+
+        print ("loading classifier model...")
+        serializers.load_npz(model_path+model_name, model)
+        optimizer = chainer.optimizers.Adam()
+        optimizer.setup(model)
+
+        self.model = model
+
+    def __call__(self, img):
+
+        img = cv2.resize(img, (self.size,self.size), interpolation = cv2.INTER_LINEAR)
+        img = img.reshape((1,3,self.size,self.size)).astype(np.float32) / 255.
+
+        validation_output = self.model.forward(chainer.Variable(img))
+        label =  np.argmax(validation_output.data)
+
+        return label
