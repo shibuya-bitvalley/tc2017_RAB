17930ca9a8392c6fb2d893fad0fa2720cf02e150
shibuya-bitvalley <bitvalley0310@docomo.ne.jp>
2017-09-13 10:36:58 +0900

    RAB

--- icart-mini-src/icart_pkg/src/rightlaser_to_lrf.cpp
+++ icart-mini-src/icart_pkg/src/rightlaser_to_lrf.cpp
@@ -2,7 +2,7 @@
 #include <tf/transform_broadcaster.h>
 
 int main(int argc, char** argv){
-   ros::init(argc, argv, "tf_broadcaster_left_urg");
+   ros::init(argc, argv, "tf_broadcaster_right_urg");
    ros::NodeHandle node;
    
 
@@ -11,9 +11,9 @@ int main(int argc, char** argv){
    
    ros::Rate rate(10.0);
    while(node.ok()){
-      transform.setOrigin(tf::Vector3(-1.0, 0.0, 0.0) );
-      transform.setRotation(tf::Quaternion(0, 0, -0.707, 0.707) );
-      br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "left_laser", "odom") );
+      transform.setOrigin(tf::Vector3(0.0, 1.0, 0.0) );
+      transform.setRotation(tf::Quaternion(0, 0, 0.707, 0.707) );
+      br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "right_laser", "right_lrf") );
       rate.sleep();
    }
    
