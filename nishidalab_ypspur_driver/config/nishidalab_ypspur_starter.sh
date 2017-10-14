378ba12e7cdbd770f747d6a7080162991f5de2ba
shibuya-bitvalley <bitvalley0310@docomo.ne.jp>
2017-10-14 10:10:21 +0900

    ypspur modified

--- nishidalab_ypspur_driver/config/nishidalab_ypspur_starter.sh
+++ nishidalab_ypspur_driver/config/nishidalab_ypspur_starter.sh
@@ -1,2 +1,1 @@
-sudo chmod 777 /dev/ttyACM0
-ypspur-coordinator -p red-air-buggy.prm -d /dev/ttyACM0 --high-resolution
+ypspur-coordinator -p red-air-buggy.prm -d /dev/serial/by-id/usb-T-frog_project_T-frog_Driver-if00
